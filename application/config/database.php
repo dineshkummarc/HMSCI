3094ffcf1002555e9f305bab0d878643c734a79f
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-15 01:27:18 +0530

    added

--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,7 @@ $db['default']['password'] = '';
 $db['default']['database'] = 'hmsci';
 
 
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
